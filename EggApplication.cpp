8f54878052ec6a1fb2ae51ebcf5becf790077e09
BrodatySzogunik <Jakub.mordalski2000@wp.pl>
2022-12-03 19:14:03 +0100

    add camera movement and option to display teapot

--- EggApplication.cpp
+++ EggApplication.cpp
@@ -14,10 +14,37 @@
 # define M_PI           3.14159265358979323846
 typedef float point3[3];
 
+
+static GLfloat viewer[] = { 0.0, 0.0, 10.0 };
+
+
+static GLfloat pix2angleX;     // przelicznik pikseli na stopnie
+static GLfloat pix2angleY;     // przelicznik pikseli na stopnie
+
+static GLint status = 0;       // stan klawiszy myszy
+                               // 0 - nie naciśnięto żadnego klawisza
+                               // 1 - naciśnięty zostać lewy klawisz
+
+static GLint rotationStatus = 0; //typ obrotu
+                                //0 - obrót obiektu
+                                //1 - obrót kamery
+
+static int up = 1;
+static float cameraTheta;
+static float cameraPhi;
+
+static int R = 4;
+
+static int x_pos_old = 0;       // poprzednia pozycja kursora myszy
+static int delta_x = 0;        // różnica pomiędzy pozycją bieżącą
+static int y_pos_old = 0;       // poprzednia pozycja kursora myszy
+static int delta_y = 0;        // różnica pomiędzy pozycją bieżącą
+                                      // i poprzednią kursora myszy
+
+
 /*************************************************************************************/
 
-// Funkcja rysująca osie układu współrzędnych
-
+//struktura punktu wraz z kolorem
 struct Point {
     float x;
     float y;
@@ -28,17 +55,55 @@ struct Point {
     float B;
 };
 
-//dzielna podziałów boku kwadratu jednostkoweg
-const int N = 50
-;
-//tablica przechowująca punkty jajka
+//dzielna podziału boku kwadratu jednostkowego
+static int layers = 4;
+static int N = layers*2 - 2;
+//tablica przechowująca punkty modelu jajka
 struct Point EggPoints[N][N];
 //zmienna odpowiedzialna za aktualny model
-int model = 1;  // 1- punkty, 2- siatka, 3 - wypełnione trójkąty
+int model = 1;  // 1- punkty, 2- siatka, 3 - wypełnione trójkąty, 4 - teapot
 
 static GLfloat theta[] = { 0.0, 0.0, 0.0 }; // trzy kąty obrotu
 
 float step = 1.0 / N;
+
+
+void Mouse(int btn, int state, int x, int y)
+{
+
+
+    if (btn == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+    {
+        x_pos_old = x;        // przypisanie aktualnie odczytanej pozycji kursora
+        y_pos_old = y;
+        // jako pozycji poprzedniej
+        status = 1;          // wcięnięty został lewy klawisz myszy
+    }
+    else if (btn == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
+        y_pos_old = y;
+        status = 2;
+    }
+    else
+        status = 0;          // nie został wcięnięty żaden klawisz
+}
+
+/*************************************************************************************/
+// Funkcja "monitoruje" położenie kursora myszy i ustawia wartości odpowiednich
+// zmiennych globalnych
+
+void Motion(GLsizei x, GLsizei y)
+{
+
+    delta_x = x - x_pos_old;     // obliczenie różnicy położenia kursora myszy
+
+    x_pos_old = x;            // podstawienie bieżącego położenia jako poprzednie
+
+    delta_y = y - y_pos_old;     // obliczenie różnicy położenia kursora myszy
+
+    y_pos_old = y;            // podstawienie bieżącego położenia jako poprzednie
+
+    glutPostRedisplay();     // przerysowanie obrazu sceny
+}
 
 
 void Axes(void)
@@ -79,15 +144,28 @@ void Axes(void)
     glVertex3fv(z_max);
 
     glEnd();
-
 }
 
 /*************************************************************************************/
 
-// Funkcja określająca co ma być rysowane (zawsze wywoływana gdy trzeba
-// przerysować scenę)
+void calculateAngles() {
+    cameraTheta += delta_x;
+    cameraPhi += delta_y;
 
+    if (cameraTheta < 0.0) {
+        cameraTheta = 360.0;
+    }
+    if (cameraTheta > 360) {
+        cameraTheta = 0;
+    }
 
+    if (cameraPhi < 0.0) {
+        cameraPhi = 360.0;
+    }
+    if (cameraPhi > 360) {
+        cameraPhi = 0;
+    }
+}
 
 
 float generateX(float u, float v) {
@@ -112,7 +190,8 @@ void GenerateEggPoints() {
             EggPoints[i][j].y = generateY(i * step, j * step);
             EggPoints[i][j].z = generateZ(i * step, j * step);
 
-            //Kolorki
+            std::cout << " x: " << EggPoints[i][j].x << " y: " << EggPoints[i][j].y << " z: " << EggPoints[i][j].x << "\n";
+
             EggPoints[i][j].R = ((double)rand() / (RAND_MAX));
             EggPoints[i][j].G = ((double)rand() / (RAND_MAX));
             EggPoints[i][j].B = ((double)rand() / (RAND_MAX));
@@ -125,24 +204,27 @@ void GenerateEggPoints() {
 void spinEgg()
 {
 
-    theta[0] -= 0.05;
-    if (theta[0] > 360.0) theta[0] -= 360.0;
+    if (status != 1 && status != 2) {
+        theta[0] += 0.05;
+        if (theta[0] > 360.0) theta[0] = 0.0;
 
-    theta[1] -= 0.05;
-    if (theta[1] > 360.0) theta[1] -= 360.0;
+        theta[1] += 0.05;
+        if (theta[1] > 360.0) theta[1] = 0.0;
 
-    theta[2] -= 0.05;
-    if (theta[2] > 360.0) theta[2] -= 360.0;
+        theta[2] += 0.05;
+        if (theta[2] > 360.0) theta[2] = 0.0;
+
+    
 
     glutPostRedisplay(); //odświeżenie zawartości aktualnego okna
+    }
 }
 
 void Egg() {
-
     switch (model)
     {
     case 1:
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N ; i++) {
             for (int j = 0; j < N; j++) {
                 glBegin(GL_POINTS);
                 glColor3f(1.0f, 0.0f, 0.0f);
@@ -352,11 +434,10 @@ void Egg() {
 
         glEnd();
         break;
-    case 4:
-        glutWireTeapot(3.0);
+        case 4:
+            glutWireTeapot(3.0);
         break;
-
-    default:
+        default:
         break;
     }
 
@@ -366,42 +447,67 @@ void Egg() {
 
 void RenderScene(void)
 {
-
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     // Czyszczenie okna aktualnym kolorem czyszczącym
 
     glLoadIdentity();
     // Czyszczenie macierzy bieżącej
 
+    if (status == 2) {
+        R += delta_y;
+        viewer[0] = cos(cameraTheta * M_PI / 180) * cos(cameraPhi * M_PI / 180) * R;
+        viewer[1] = sin(cameraPhi * M_PI / 180) * R;
+        viewer[2] = sin(cameraTheta * M_PI / 180) * cos(cameraPhi * M_PI / 180) * R;
+    }
 
+    if (status == 1 && rotationStatus == 1)                     // jeśli lewy klawisz myszy wcięnięty
+    {
+        calculateAngles();
+        viewer[0] = cos(cameraTheta * M_PI / 180) * cos(cameraPhi * M_PI / 180) * R;
+        viewer[1] = sin(cameraPhi * M_PI / 180) * R;
+        viewer[2] = sin(cameraTheta * M_PI / 180) * cos(cameraPhi * M_PI / 180) * R;
 
+        if (cameraPhi > 90 && cameraPhi < 270) {
+            up = -1;
+        }
+        else {
+            up = 1;
+        }
 
-    glColor3f(1.0f, 1.0f, 1.0f);
-
-
-    glRotatef(theta[0], 1.0, 0.0, 0.0);
-
-    glRotatef(theta[1], 0.0, 1.0, 0.0);
-
-    glRotatef(theta[2], 0.0, 0.0, 1.0);
-
-    Axes();
-    // Narysowanie osi przy solverocy funkcji zdefiniowanej wyżej
-
-    Egg();
-
+    }
 
     
+    gluLookAt(viewer[0], viewer[1], viewer[2], 0.0, 0.0, 0.0, 0.0, up, 0.0);
+    // Zdefiniowanie położenia obserwatora
 
+    Axes();
+    // Narysowanie osi przy pomocy funkcji zdefiniowanej powyżej 
+
+
+
+    if (status == 1 && rotationStatus == 0)                     // jeśli lewy klawisz myszy wcięnięty
+    {
+        theta[0] += delta_y * pix2angleY;    // modyfikacja kąta obrotu o kat proporcjonalny
+        
+        theta[1] += delta_x * pix2angleX;    // modyfikacja kąta obrotu o kat proporcjonalny
+        }
+
+        glRotatef(theta[0], 1.0, 0.0, 0.0);
+
+        glRotatef(theta[1], 0.0, 1.0, 0.0);
+
+        glRotatef(theta[2], 0.0, 0.0, 1.0);
+    
+
+
+
+    Egg();
 
     glFlush();
     // Przekazanie poleceń rysujących do wykonania
 
-
-
     glutSwapBuffers();
     //
-
 }
 
 /*************************************************************************************/
@@ -430,45 +536,34 @@ void MyInit(void)
 void ChangeSize(GLsizei horizontal, GLsizei vertical)
 {
 
-    GLfloat AspectRatio;
-    // Deklaracja zmiennej AspectRatio  określającej proporcję
-    // wymiarów okna 
 
-    if (vertical == 0)  // Zabezpieczenie przed dzieleniem przez 0
 
-        vertical = 1;
-
-    glViewport(0, 0, horizontal, vertical);
-    // Ustawienie wielkościokna okna widoku (viewport)
-    // W tym przypadku od (0,0) do (horizontal, vertical)  
+    pix2angleX = 360.0 / (float)horizontal;  // przeliczenie pikseli na stopnie
+    pix2angleY = 360.0 / (float)horizontal;  // przeliczenie pikseli na stopnie
 
     glMatrixMode(GL_PROJECTION);
-    // Przełączenie macierzy bieżącej na macierz projekcji 
+    // Przełączenie macierzy bieżącej na macierz projekcji
 
     glLoadIdentity();
-    // Czyszcznie macierzy bieżącej            
+    // Czyszcznie macierzy bieżącej
 
-    AspectRatio = (GLfloat)horizontal / (GLfloat)vertical;
-    // Wyznaczenie współczynnika  proporcji okna
-    // Gdy okno nie jest kwadratem wymagane jest określenie tak zwanej
-    // przestrzeni ograniczającej pozwalającej zachować właściwe
-    // proporcje rysowanego obiektu.
-    // Do okreslenia przestrzeni ograniczjącej służy funkcja
-    // glOrtho(...)            
+    gluPerspective(70, 1.0, 1.0, 30.0);
+    // Ustawienie parametrów dla rzutu perspektywicznego
+
 
     if (horizontal <= vertical)
-
-        glOrtho(-7.5, 7.5, -7.5 / AspectRatio, 7.5 / AspectRatio, 10.0, -10.0);
+        glViewport(0, (vertical - horizontal) / 2, horizontal, horizontal);
 
     else
-
-        glOrtho(-7.5 * AspectRatio, 7.5 * AspectRatio, -7.5, 7.5, 10.0, -10.0);
+        glViewport((horizontal - vertical) / 2, 0, vertical, vertical);
+    // Ustawienie wielkości okna okna widoku (viewport) w zależności
+    // relacji pomiędzy wysokością i szerokością okna
 
     glMatrixMode(GL_MODELVIEW);
-    // Przełączenie macierzy bieżącej na macierz widoku modelu                                    
+    // Przełączenie macierzy bieżącej na macierz widoku modelu  
 
     glLoadIdentity();
-    // Czyszcenie macierzy bieżącej
+    // Czyszczenie macierzy bieżącej
 
 }
 
@@ -478,11 +573,20 @@ void ChangeSize(GLsizei horizontal, GLsizei vertical)
 
 void keys(unsigned char key, int x, int y)
 {
-    if (key == 'p') model = 1;
+    if (key == 'q') model = 1;
     if (key == 'w') model = 2;
-    if (key == 's') model = 3;
+    if (key == 'e') model = 3;
+    if (key == 'r') model = 4;
+    if (key == 'u') {
+        layers++;
+        N
+    }
+    if (key == 'c') {
+        rotationStatus = rotationStatus == 1 ? 0 : 1;
+    }
 
-    RenderScene(); // przerysowanie obrazu sceny
+
+    RenderScene(); 
 }
 
 
@@ -498,19 +602,22 @@ void main(void)
     GenerateEggPoints();
 
     glutDisplayFunc(RenderScene);
-    // Określenie, że funkcja RenderScene będzie funkcją zwrotną
-    // (callback function).  Bedzie ona wywoływana za każdym razem
-    // gdy zajdzie potrzba przeryswania okna 
-
-    glutReshapeFunc(ChangeSize);
-    // Dla aktualnego okna ustala funkcję zwrotną odpowiedzialną
-    // zazmiany rozmiaru okna      
+    glutReshapeFunc(ChangeSize);    
 
 
     glutKeyboardFunc(keys);
+
+    glutMouseFunc(Mouse);
+    // Ustala funkcję zwrotną odpowiedzialną za badanie stanu myszy
+
+    glutMotionFunc(Motion);
+    // Ustala funkcję zwrotną odpowiedzialną za badanie ruchu myszy
+
+
     
 
     glutIdleFunc(spinEgg);
+
 
     MyInit();
     // Funkcja MyInit() (zdefiniowana powyżej) wykonuje wszelkie
